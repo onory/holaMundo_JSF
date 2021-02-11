10806ff51289b2440baa4c60af3c1c75ac74d9c4
onory <onory@DESKTOP-4HGOQO2>
2021-02-11 14:35:48 -0600

    Revert "Hasta clase 272"
    This reverts commit 0d9a0c4fdb362fc9aac695fe64154816762ba90d.

--- src/main/java/beans/backing/VacanteForm.java
+++ src/main/java/beans/backing/VacanteForm.java
@@ -6,8 +6,6 @@ import beans.model.Candidato;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Named // etiqueta par ser reconocido el manage bean por JSF
 @RequestScoped // determina el alcance
@@ -17,8 +15,6 @@ public class VacanteForm {
     
 @Inject // dependencia de CDI para poder ser usado desde paquete de model     
 private Candidato candidato;   
-
-Logger log = LogManager.getRootLogger();
     
 public void setCandidato(Candidato candidato){
     
@@ -30,14 +26,9 @@ public void setCandidato(Candidato candidato){
 public String enviar(){
     
     if(this.candidato.getNombre().equals("Juan")){ // valida si el nombre ingresado en index es juan para redireccionar
-       
-        log.info("Entrando al caso de exito");
-        
+    
     return "exito"; // exito refiere a una xhtml, no es necesario mencionar la extencion
 }else{
-        
-        log.info("Entrando al caso de fallo");
-        
         return "fallo"; // fallo refiere a una xhtml, no es necesario mencionar la extencion
     }
     
